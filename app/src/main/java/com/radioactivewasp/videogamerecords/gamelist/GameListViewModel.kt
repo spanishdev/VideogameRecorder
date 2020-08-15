9c78c7448c426f04ee8ce350ff8f9bee690f1a89
Jordi <spanishdev@gmail.com>
2020-08-15 20:56:35 +0200

    Add GameRepository

--- app/src/main/java/com/radioactivewasp/videogamerecords/gamelist/GameListViewModel.kt
+++ app/src/main/java/com/radioactivewasp/videogamerecords/gamelist/GameListViewModel.kt
@@ -1,17 +1,16 @@
 package com.radioactivewasp.videogamerecords.gamelist
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.radioactivewasp.videogamerecords.dummy.DummyContent
 import com.radioactivewasp.videogamerecords.gamelist.ui.GameListItem
 import com.radioactivewasp.videogamerecords.gamelist.ui.toGameItem
 import com.radioactivewasp.videogamerecords.model.data.Game
+import com.radioactivewasp.videogamerecords.model.repository.GameRepository
 
-class GameListViewModel: ViewModel() {
+class GameListViewModel(private val repository: GameRepository): ViewModel() {
     val gameData: MutableLiveData<List<GameListItem>> = MutableLiveData()
 
-    fun requestGames() {
-        gameData.postValue(DummyContent.items.map(Game::toGameItem))
+    fun requestGames() = repository.retrieveGames().apply {
+        gameData.postValue(map(Game::toGameItem))
     }
 }
--- app/src/main/java/com/radioactivewasp/videogamerecords/model/repository/GameRepository.kt
+++ app/src/main/java/com/radioactivewasp/videogamerecords/model/repository/GameRepository.kt
@@ -1,4 +1,8 @@
 package com.radioactivewasp.videogamerecords.model.repository
 
+import com.radioactivewasp.videogamerecords.dummy.DummyContent
+import com.radioactivewasp.videogamerecords.model.data.Game
+
 class GameRepository {
+    fun retrieveGames(): List<Game> = DummyContent.items
 }
